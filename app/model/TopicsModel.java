81766f4b2bd798929e2de2a85637f207b3024f2f
pooja14898 <pooja14898@gmail.com>
2021-11-14 18:07:13 -0500

    added topics model

---
+++ app/model/TopicsModel.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.ArrayList;
+
+public class TopicsModel {
+private ArrayList<String> topics;
+
+public ArrayList<String> getTopics() {
+	return topics;
+}
+
+public void setTopics(ArrayList<String> topics) {
+	this.topics = topics;
+}
+}
